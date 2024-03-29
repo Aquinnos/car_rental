c46fb8ad108e4654eb303860fb869aa8b77767d9
Anagdok <154678228+Anagdok@users.noreply.github.com>
2024-03-29 17:02:59 +0100

    Add files via upload

--- car_rental/Form1.cs
+++ car_rental/Form1.cs
@@ -10,14 +10,18 @@ using System.Windows.Forms;
 
 namespace car_rental
 {
-    public partial class Form1 : Form
+    public partial class glowna : Form
     {
         private DatabaseManager dbManager;
 
-        public Form1()
+        public glowna()
         {
             InitializeComponent();
             dbManager = new DatabaseManager();
+            Panel_oferty.Visible = false;
+            panel_login.Visible = false;
+            panel_rejstracja.Visible = false;
+            panel_profil.Visible = false;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -30,6 +34,46 @@ namespace car_rental
             {
                 // Połączenie nie zostało nawiązane
             }
+        }
+
+
+        private void Login_rejstracja_Click(object sender, EventArgs e)
+        {
+            panel_login.Visible = true;
+            panel_glowny.Visible = false;
+
+        }
+
+        private void oferty_Click(object sender, EventArgs e)
+        {
+            Panel_oferty.Visible = true;
+            panel_glowny.Visible = false;
+        }
+
+        private void profil_Click(object sender, EventArgs e)
+        {
+            panel_profil.Visible = true;
+            panel_glowny.Visible = false;
+        }
+
+        private void powrot_Click(object sender, EventArgs e)
+        {
+            panel_glowny.Visible = true;
+            Panel_oferty.Visible = false;
+            panel_login.Visible = false;
+            panel_profil.Visible = false;
+        }
+
+        private void powrot_do_login_Click(object sender, EventArgs e)
+        {
+            panel_login.Visible = true;
+            panel_rejstracja.Visible = false;
+        }
+
+        private void przejscie_do_rejstracji_Click(object sender, EventArgs e)
+        {
+            panel_login.Visible = false;
+            panel_rejstracja.Visible = true;
         }
     }
 }
