179feb74fc07efe6165a115060a55c1a510cdb61
София Станкевич <xpchikovna@gmail.com>
2024-05-06 16:01:57 +0200

    rejestracja1

---
+++ car_rental/registration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace car_rental
+{
+    public partial class registration : Form
+    {
+        public registration()
+        {
+            InitializeComponent();
+        }
+
+        private void registration_Load(object sender, EventArgs e)
+        {
+            string hexColor = "#FFFAE2";
+            Color myColor = ColorTranslator.FromHtml(hexColor);
+            this.BackColor = myColor;
+        }
+    }
+}
